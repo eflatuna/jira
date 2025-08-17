175c4b79c7b3bf2bce049fbfde9f3a60566d1280
esra <esrabaskaya84@gmail.com>
2025-08-17 23:25:31 +0200

    `Added CSS styles to AddTask.vue view`

--- src/views/AddTask.vue
+++ src/views/AddTask.vue
@@ -19,4 +19,50 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+form {
+	box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+	padding: 20px;
+	margin: 20px 0px;
+	background-color: white;
+	border-radius: 20px;
+}
+label {
+	display: block;
+	margin: 20px 0px 10px;
+	letter-spacing: 1px;
+	font-size: 15px;
+	font-weight: 500;
+}
+input {
+	padding: 10px;
+	font-size: 16px;
+	border: 1px solid #ddd;
+	border-radius: 4px;
+	width: 100%;
+	height: 40px;
+	margin-bottom: 20px;
+	outline: none;
+}
+textarea {
+	padding: 10px;
+	font-size: 20px;
+	border: 1px solid #ddd;
+	border-radius: 4px;
+	width: 100%;
+	height: 100px;
+	margin-bottom: 20px;
+	outline: none;
+}
+button {
+	background-color: #4caf50;
+	color: white;
+	padding: 14px 20px;
+	margin: 8px 0;
+	border: none;
+	cursor: pointer;
+	width: 100%;
+	border-radius: 4px;
+	font-size: 16px;
+}
+</style>
