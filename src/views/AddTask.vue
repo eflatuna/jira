16fd8702cb8b4ca80ea6a05b05f399742c28d11d
esra <esrabaskaya84@gmail.com>
2025-08-17 23:17:02 +0200

    `Added new route for AddTask view`

---
+++ src/views/AddTask.vue
@@ -0,0 +1,22 @@
+<template>
+	<form>
+		<label>Title</label>
+		<input type="text" v-model="title" required />
+		<label>Task Details</label>
+		<textarea v-model="details" required></textarea>
+		<button>Add</button>
+	</form>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: "",
+			details: "",
+		};
+	},
+};
+</script>
+
+<style></style>
