e20ae6f001f6786c50498a94900c895ec3082398
esra <esrabaskaya84@gmail.com>
2025-08-17 13:19:43 +0200

    `feat: Added task list display and API call to fetch tasks in HomeView.vue and updated vue.config.js`

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,25 @@
-<template>Hi!</template>
+<template>
+	<div v-if="tasks.length">
+		<div v-for="task in tasks" key="task.id">
+			<p>{{ task.title }}</p>
+		</div>
+	</div>
+</template>
 
 <script>
 export default {
 	name: "HomeView",
 	components: {},
+	data() {
+		return {
+			tasks: [],
+		};
+	},
+	mounted() {
+		fetch("http://localhost:3000/tasks")
+			.then((res) => res.json())
+			.then((data) => (this.tasks = data))
+			.catch((err) => console.log(err.message));
+	},
 };
 </script>
