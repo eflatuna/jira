01adb52702c168459198edab3c544cc177c4a0ac
esra <esrabaskaya84@gmail.com>
2025-08-19 23:05:20 +0200

    `Added form to EditTask.vue for editing tasks, including title and details fields, and a mounted lifecycle hook to fetch task data from the server.`

--- src/views/EditTask.vue
+++ src/views/EditTask.vue
@@ -1,9 +1,29 @@
 <template>
-	<div>aaaa</div>
+	<form @submit.prevent="handleSubmit()">
+		<label>Title</label>
+		<input type="text" v-model="title" required />
+		<label>Task Details</label>
+		<textarea v-model="details" required></textarea>
+		<button>Add</button>
+	</form>
 </template>
 
 <script>
-export default {};
+export default {
+	props: ["id"],
+	data() {
+		return {
+			title: "",
+			details: "",
+		};
+	},
+	mounted() {
+		fetch("http://localhost:3000/tasks/" + this.id)
+			.then((res) => res.json())
+			.then((data) => console.log(data))
+			.catch((err) => console.log(err.message));
+	},
+};
 </script>
 
 <style></style>
